f3f26d1a8a3c5038f0b27d9c7b2afa452e39f6de
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-14 16:49:26 +0300

    gameboard class has been implemented , unit testing has been performed and was successful moving on to Player class next

---
+++ src/gameboard.js
@@ -0,0 +1,74 @@
+class GameBoard {
+  constructor() {
+    this.gridSize = 10;
+    this.ships = [];
+    this.missedAttacks = [];
+    this.successfulAttacks = [];
+    this.board = this.createEmptyBoard();
+  }
+
+  createEmptyBoard() {
+    const board = [];
+    for (let i = 0; i < this.gridSize; i++) {
+      const row = [];
+      for (let j = 0; j < this.gridSize; j++) {
+        row.push(null);
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  placeShip(x, y, ship, isHorizontal) {
+    let ship_length = ship.length;
+    const new_coordinates = [];
+
+    // we need to check if the coordinates are correct and if there is any overlap with other ships which just means that all the squares should be null to place it there
+
+    if (isHorizontal) {
+      for (let i = 0; i < ship_length; i++) {
+        if (y + i >= this.gridSize || this.board[x][y + 1] !== null) {
+          return false;
+        }
+        new_coordinates.push([x, y + i]);
+      }
+    } else {
+      for (let i = 0; i < ship_length; i++) {
+        if (x + i >= this.gridSize || this.board[x + i][y] !== null) {
+          return false;
+        }
+        new_coordinates.push([x + i, y]);
+      }
+    }
+
+    for (const [r, c] of new_coordinates) {
+      this.board[r][c] = ship;
+    }
+
+    this.ships.push(ship);
+    return true;
+  }
+
+  receiveAttack(x, y) {
+    let target_coordinates = this.board[x][y];
+    if (target_coordinates === null) {
+      this.missedAttacks.push([x, y]);
+      return false;
+    } else {
+      this.successfulAttacks.push([x, y]);
+      target_coordinates.hit();
+      return true;
+    }
+  }
+
+  allShipsSunk() {
+    for (const ship of this.ships) {
+      if (!ship.isSunk()) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+module.exports = GameBoard;
--- src/unit.test.js
+++ src/unit.test.js
@@ -1,4 +1,5 @@
 const ship = require("./ship");
+const GameBoard = require("./gameboard");
 
 describe("Main tests", () => {
   describe("Ship Object works correctly", () => {
@@ -32,6 +33,58 @@ describe("Main tests", () => {
       test_ship.hit();
       test_ship.isSunk();
       expect(test_ship.sunk).toBe(true);
+    });
+  });
+
+  describe("GameBoard works correctly", () => {
+    let test_gameBoard;
+    let test_ship;
+
+    beforeEach(() => {
+      test_gameBoard = new GameBoard();
+      test_ship = new ship(3, 3, false);
+    });
+
+    test("GameBoard is created and everything inside is null", () => {
+      expect(test_gameBoard.gridSize).toBe(10);
+      expect(test_gameBoard.ships).toStrictEqual([]);
+      expect(test_gameBoard.missedAttacks).toStrictEqual([]);
+      expect(test_gameBoard.successfulAttacks).toStrictEqual([]);
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          expect(test_gameBoard.board[i][j]).toBe(null);
+        }
+      }
+    });
+
+    test("GameBoard correctly places ships", () => {
+      test_gameBoard.placeShip(0, 0, test_ship, true);
+      expect(test_gameBoard.board[0][0]).toBe(test_ship);
+      expect(test_gameBoard.board[0][1]).toBe(test_ship);
+      expect(test_gameBoard.board[0][2]).toBe(test_ship);
+      expect(test_gameBoard.board[0][3]).toBe(null);
+      expect(test_gameBoard.ships[0]).toBe(test_ship);
+    });
+
+    test("GameBoard receives attacks and recognizes if they hit or not", () => {
+      test_gameBoard.placeShip(0, 0, test_ship, true);
+      expect(test_gameBoard.receiveAttack(0, 1)).toBe(true);
+      expect(test_gameBoard.receiveAttack(3, 1)).toBe(false);
+    });
+
+    test("GameBoard recognizes when all ships have been sunk", () => {
+      test_gameBoard.placeShip(0, 0, test_ship, true);
+
+      for (let i = 0; i < test_gameBoard.ships.length; i++) {
+        expect(test_gameBoard.allShipsSunk()).toBe(false);
+      }
+
+      test_ship.hit();
+      test_ship.hit();
+      test_ship.hit();
+      for (let j = 0; j < test_gameBoard.ships.length; j++) {
+        expect(test_gameBoard.allShipsSunk()).toBe(true);
+      }
     });
   });
 });
